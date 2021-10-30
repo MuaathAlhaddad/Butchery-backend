ca614338db1ee0cf861c9b199349c044947e2580
Muaadh Esmail Qaid Almrham <muaath2000@gmail.com>
2021-10-30 09:44:14 +0800

    feat: fix resource delete temporary in admin

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -81,7 +81,9 @@
                                                 <form action="{{ route('users.destroy', $user->id) }}" method="POST" style="display: inline-block;">
                                                     <input type="hidden" name="_method" value="DELETE">
                                                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                                    <i class="far fa-trash-alt text-danger delete-btn" style=" cursor: pointer;"></i>
+                                                     <button type="submit" class="btn btn-link">
+                                                            <i class="far fa-trash-alt text-danger delete-btn" style=" cursor: pointer;"></i>
+                                                        </button>
                                                 </form>
                                             @endcan
 
